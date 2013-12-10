3281d0ba8e7709cca18d18a5a5cc92dfd868ba92
buptjz <buptjz@gmail.com>
2013-12-10 14:08:53 +0800

    bug修复：修改获取当前显示状态

--- GuessWord/Model/PlayBoard.h
+++ GuessWord/Model/PlayBoard.h
@@ -9,10 +9,8 @@
 #import <Foundation/Foundation.h>
 #import "Word.h"
 
-enum DIRECTION{
-    VERTIVAL = 0,
-    HORIZANTAL
-};
+#define VERTICAL_DIRECTION YES;
+#define HORIZONTAL_DERECTION NO;
 
 #define BLOCK @"#" //不能填写的位置
 #define BLANK @"-"  //空着的位置
@@ -23,13 +21,15 @@ enum DIRECTION{
 
 /**************************************************API******************************************/
 
-/*Controller调用方式：先用initWith初始化，然后每次用户输入更新Board，获取current_state*/
+/*Controller调用方式：先用initWith初始化，然后每次用户输入更新Board，获取current_state
+
+ */
 
 -(void)updateBoardWithInputValue:(NSString *)oneAlphabet atPoint:(CGPoint)point;//必须调用！每次用户输入一个字母
 
 -(PlayBoard *)initWith;//默认的初始化函数
--(NSArray *)current_state;//获取当前游戏状态：
--(Word *)wordOfPoint:(CGPoint *)point inDirection:(BOOL)direction;//通过point获得指定方向的单词
+-(NSArray *)current_state;//获取当前游戏显示状态：
+-(Word *)wordOfPoint:(CGPoint)point inDirection:(BOOL)direction;//通过point获得指定方向的单词
 -(BOOL)isBingoOfWordAtPoint:(CGPoint)point inDirection:(BOOL)direction;//判断某个点所在单词是否完成
 -(BOOL)isFinished;//是否闯关成功
 -(BOOL)isClickableOfPoint:(CGPoint)point;//判断该点是否能够点击
--- GuessWord/Model/PlayBoard.m
+++ GuessWord/Model/PlayBoard.m
@@ -1,9 +1,10 @@
 //
-//  PlayBoard.m
-//  DMTXW
 //
-//  Created by WangJZ on 12/9/13.
-//  Copyright (c) 2013 WangJZ. All rights reserved.
+//  【重要信息】
+//  从tmp读取input，将input写入文件中的tmp
+//  每次输入改变input
+//  根据每个word的的坐标找到对应的input和correct，改变display
+//
 //
 
 #import "PlayBoard.h"
@@ -28,11 +29,12 @@
 @property(nonatomic,strong)NSMutableArray *areaOfDisplay;//应该显示的内容，目前看来没啥用处，可以考虑删掉。。。
 
 
-/*********************************私有函数*************************/
+/*********************************私有API*************************/
 -(void)initAreaOfInputAndAreaOfCorrectBasedOnTMP;//根据tmp信息初始化两个area数组
 
 -(CGPoint)nextPointFromPoint:(CGPoint)fromPoint;//找到一个点的下一个点
-
+-(void) updateAreaOfDisplayByWord:(Word *)word;//根据一个word更新areaOfDisplay
+-(BOOL)isBingoOfWord:(Word *)word;//查看某个单词是否完成
 -(PlayBoard *)readFromFile:(NSString *)fromFile;//根据信息生成PlayBoard
 -(void)saveToFile:(NSString *)saveFile;//将信息保存到文件中（或数据库）
 
@@ -40,6 +42,45 @@
 @end
 
 @implementation PlayBoard
+
+//在某个坐标上输入一个字母，修改areaOfInput
+-(void)updateBoardWithInputValue:(NSString *)oneAlphabet atPoint:(CGPoint)point{
+    int x = (int)point.x;
+    int y = (int)point.y;
+    
+    //check坐标在方格内
+    if (x>= 0 && x< self.width && y>= 0 && y <self.height ) {
+        
+        //if (![self.areaOfCorrect[y][x] isEqualToString:BLOCK] && ![self.areaOfInput[y][x] isEqualToString:self.areaOfCorrect[y][x]]) {
+        
+        //check该坐标位置不是Block,暂时不检查是否已经是正确答案
+        if (![self.areaOfCorrect[y][x] isEqualToString:BLOCK]) {
+            self.areaOfInput[y][x] = oneAlphabet;
+        }
+    }
+}
+
+//指定的初始化函数
+-(PlayBoard *)initWith{
+    self = [super init];
+    if (self) {
+        Word *word1 = [[Word alloc]init];
+        word1.answer_capital = @"hyl";
+        word1.answer_chinese_character = @"好运来";
+        word1.mask = @"100";
+        word1.description = @"时来运转";
+        word1.tmp = @"h-l";
+        word1.horizontal = YES;
+        word1.start_x = 1;
+        word1.start_y = 2;
+        word1.length = 3;
+        self.words = @[word1];
+        self.width = 5;
+        self.height = 5;
+        [self initAreaOfInputAndAreaOfCorrectBasedOnTMP];
+    }
+    return self;
+}
 
 //为了打印输出查看结果
 -(NSString*)description
@@ -56,6 +97,15 @@
     
     [retString appendString:@"\[Input]\n"];
     for (NSArray *row_array in self.areaOfInput) {
+        for (NSString *columnString in row_array) {
+            [retString appendString:columnString];
+            [retString appendString:@" "];
+        }
+        [retString appendString:@"\n"];
+    }
+    
+    [retString appendString:@"\[Display]\n"];
+    for (NSArray *row_array in self.areaOfDisplay) {
         for (NSString *columnString in row_array) {
             [retString appendString:columnString];
             [retString appendString:@" "];
@@ -99,7 +149,7 @@
 }
 
 
-//根据tmp信息初始化两个area数组
+//根据tmp信息初始化两个area数组，只在最初调用
 -(void)initAreaOfInputAndAreaOfCorrectBasedOnTMP{
     for (Word *aWord in self.words) {
         NSString *tmp       = aWord.tmp;
@@ -127,73 +177,104 @@
     }
 }
 
-//更新并返回得到当前应该显示的状态
--(NSArray *)current_state
+//根据一个word更新areaOfDisplay
+-(void) updateAreaOfDisplayByWord:(Word *)word
 {
-    for (Word *aWord in self.words) {
-        BOOL horizontal     = aWord.horizontal;
-        int x               = aWord.start_x;
-        int y               = aWord.start_y;
-        int length          = aWord.length;
-        for (int i = 0 ; i < length; i++) {
-            if (horizontal) {
-                if (y >= 0 && y < self.height && x+i >= 0 && x+i < self.width) {
-                    //应该显示的内容：当用户输入和正确答案一致时，显示汉字，否则显示用户输入
-                    if ([self.areaOfCorrect[y][x+i] isEqualToString:self.areaOfInput[y][x+i]]) {
-                        self.areaOfDisplay[y][x+i] = [aWord.answer_chinese_character substringWithRange:NSMakeRange(i,1)];
-                    } else {
-                        self.areaOfDisplay[y][x+i] = self.areaOfInput[y][x+i];
-                    }
-                }
+    //根据word的方向和该word是否bingo来确定显示的内容
+    BOOL bingo      = [self isBingoOfWord:word];
+    int x           = word.start_x;
+    int y           = word.start_y;
+    NSString *chi   = word.answer_chinese_character;
+    
+    for (int i = 0 ; i < word.length; i++) {
+        if (word.horizontal) {
+            if (bingo) {
+                self.areaOfDisplay[y][x+i] = [chi substringWithRange:NSMakeRange(i,1)];
             } else {
-                if (x >= 0 && x < self.width && y+i >= 0 && y+i < self.height) {
-                    if ([self.areaOfCorrect[y+i][x] isEqualToString:self.areaOfInput[y][x+i]]) {
-                        self.areaOfDisplay[y+i][x] = [aWord.answer_chinese_character substringWithRange:NSMakeRange(i,1)];
-                    } else {
-                        self.areaOfDisplay[y+i][x] = self.areaOfInput[y][x+i];
-                    }
-                }
+                self.areaOfDisplay[y][x+i] = self.areaOfInput[y][x+i];
             }
         }
+        else{
+            if (bingo) {
+                self.areaOfDisplay[y+i][x] = [chi substringWithRange:NSMakeRange(i,1)];
+            } else {
+                self.areaOfDisplay[y+i][x] = self.areaOfInput[y+i][x];
+            }
+        }
+    }
+}
+
+//获得该point该方向上的单词
+-(Word *)wordOfPoint:(CGPoint)point inDirection:(BOOL)isHorizontal
+{
+    //如果该点该方向上有单词，返回该单词，如果没有，返回nil
+    Word *retWord = nil;
+    int x = (int)point.x;
+    int y = (int)point.y;
+    
+    for (Word *aWrod in self.words) {
+        if (isHorizontal) {
+            //满足三个条件 1）纵坐标一致， 2）横坐标在起始和终止范围内
+            if (y == aWrod.start_y && x >= aWrod.start_x && x <= aWrod.start_x + aWrod.length) {
+                retWord = aWrod;
+                break;
+            }
+        } else {
+            if (x == aWrod.start_x && y >= aWrod.start_y && y <= aWrod.start_y + aWrod.length) {
+                retWord = aWrod;
+            }
+        }
+    }
+    return retWord;
+}
+
+//查看某个单词是否完成
+-(BOOL)isBingoOfWord:(Word *)word{
+    //根据word的坐标和横纵方向来从correct和input中确定它是否已经完成
+    BOOL bingo = YES;
+    int x = word.start_x;
+    int y = word.start_y;
+    
+    for (int i = 0 ; i < word.length; i++) {
+        if (word.horizontal) {
+            if (![self.areaOfCorrect[y][x+i] isEqualToString:self.areaOfInput[y][x+i]]) {
+                bingo = NO;
+                break;
+            }
+        } else {
+            if (![self.areaOfCorrect[y+i][x] isEqualToString:self.areaOfInput[y+i][x]]) {
+                bingo = NO;
+                break;
+            }
+        }
+    }
+    return bingo;
+}
+
+
+//判断某个点所在单词是否完成
+-(BOOL)isBingoOfWordAtPoint:(CGPoint)point inDirection:(BOOL)isHorizontal{
+    //先获取当前的点上对应的那两个词
+    Word *word = [self wordOfPoint:point inDirection:isHorizontal];
+    //查看对应的单词是否完成
+    if ([self isBingoOfWord:word]) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+//Lazy更新,返回得到当前应该显示的状态
+-(NSArray *)current_state
+{
+    //batch update
+    for (Word *aWord in self.words) {
+        [self updateAreaOfDisplayByWord:aWord];
     }
     return self.areaOfDisplay;
 }
 
-//在某个坐标上输入一个字母
--(void)updateBoardWithInputValue:(NSString *)oneAlphabet atPoint:(CGPoint)point{
-    int x = (int)point.x;
-    int y = (int)point.y;
-    
-    //check坐标在方格内
-    if (x>= 0 && x< self.width && y>= 0 && y <self.height ) {
-        //check该坐标位置不是Block，不是汉字
-        if (![self.areaOfCorrect[y][x] isEqualToString:BLOCK] && ![self.areaOfInput[y][x] isEqualToString:self.areaOfCorrect[y][x]]) {
-            self.areaOfInput[y][x] = oneAlphabet;
-        }
-    }
-}
 
-//指定的初始化函数
--(PlayBoard *)initWith{
-    self = [super init];
-    if (self) {
-        Word *word1 = [[Word alloc]init];
-        word1.answer_capital = @"hyl";
-        word1.answer_chinese_character = @"好运来";
-        word1.mask = @"100";
-        word1.description = @"时来运转";
-        word1.tmp = @"h-l";
-        word1.horizontal = YES;
-        word1.start_x = 1;
-        word1.start_y = 2;
-        word1.length = 3;
-        self.words = @[word1];
-        self.width = 5;
-        self.height = 5;
-        [self initAreaOfInputAndAreaOfCorrectBasedOnTMP];
-    }
-    return self;
-}
 
 
 
