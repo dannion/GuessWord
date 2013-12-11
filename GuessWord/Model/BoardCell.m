7aba6d9f5594301f5ca4511795a9489ae79d39cf
buptjz <buptjz@gmail.com>
2013-12-11 15:17:10 +0800

    修改了PlayBoard的bug：更新显示状态将汉字给修改了

--- GuessWord/Model/BoardCell.m
+++ GuessWord/Model/BoardCell.m
@@ -10,4 +10,38 @@
 
 @implementation BoardCell
 
+-(void)setStateDoneWithChineseCharacter:(NSString *)chineseCharacter{
+    self.input = chineseCharacter;
+    self.currentState = GWGridCellCurrentStateDone;
+}
+
+-(BOOL)isCellDone{
+    if (self.currentState == GWGridCellCurrentStateDone) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+-(BOOL)isCellBlock{
+    if ([self.correct isEqualToString:BLOCK]) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+-(BOOL)isCellCorrect{
+    if ([self.input isEqualToString:self.correct]) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+-(BOOL)isCellInputBlank{
+    if ([self.input isEqualToString:BLANK]) {
+        return YES;
+    }else {
+        return NO;
+    }
+}
 @end
