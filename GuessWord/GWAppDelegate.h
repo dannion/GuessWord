663cb895e1814caa65e1ad03f81580a0cbd7dfec
buptjz <buptjz@gmail.com>
2013-12-11 16:20:33 +0800

    tmp

--- GuessWord/GWAppDelegate.h
+++ GuessWord/GWAppDelegate.h
@@ -7,9 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface GWAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property(nonatomic,strong)NSManagedObjectContext *managedObjectContext;
+@property(nonatomic,strong) NSManagedObjectModel *managedObjectModel;
+@property(nonatomic,strong)NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+-(void)saveContext;
 @end
